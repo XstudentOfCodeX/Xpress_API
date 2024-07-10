93f4fab319c6015098413bf51b14767276ca8b76
joel <hendersonjoel0@gmail.com>
2024-07-10 07:30:39 -0400

    adding sql db

---
+++ api/artist.js
@@ -0,0 +1,14 @@
+import { express } from '/server.js'
+const artistRouter = express.Router();
+
+
+
+
+
+
+
+
+
+
+
+module.exports = artistRouter;
--- migration.js
+++ migration.js
@@ -3,15 +3,25 @@ const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database('./database.sqlite');
 
 db.serialize(() => {
-    db.run('DROP TABLE Artist IF EXIST', (error) => {
+    db.run('DROP TABLE IF EXIST Artist', (error) => {
         if(error) {
             console.error(error.message);
         }
+        console.log('Connected to the mydatabase.db SQLite database.')
     })
     db.run(`CREATE TABLE Artist
         (id INTEGER PRIMARY KEY,
         name TEXT,
         date_of_birth TEXT,
-        biography 
-        )`)
+        biography TEXT,
+        is_currently_employed INTEGER DEFAULT 1
+        )`, (error) => {
+            if (error) {
+              console.error(error.message);
+            } else {
+              console.log('Table "Strip" created successfully.');
+            }
+    });
 });
+
+
