34edefba7ba6f835f93c418d334c023f36a0f32d
joel <hendersonjoel0@gmail.com>
2024-07-14 00:50:34 -0400

    adding Issues sqlite table

--- api/series.js
+++ api/series.js
@@ -11,7 +11,7 @@ seriesRouter.param('seriesId', (req, res, next, seriesId) => {
         },
         (err, series) => {
             if(err) {
-                console.error('Error in retrieving data', err);
+                console.error('Error in retrieving data');
                 next(err)
             } else if(series) {
                 req.series = series;
@@ -40,8 +40,91 @@ seriesRouter.get('/', (req, res, next) => {
 
 
 seriesRouter.get('/:seriesId', (req, res, next) => {
-    res.status(200).json({series: req.series}); 
+    res.status(200).json({ series: req.series });
+    next(); 
 });
+
+
+
+
+
+seriesRouter.post('/', (req, res, next) => {
+    const name = req.body.series.name;
+    const description = req.body.series.description;
+
+    if (!name || !description) {
+       return res.sendStatus(400);
+    }
+
+    const sql = 'INSERT INTO Series (name, description) VALUES ($name, $description)';
+    
+    const values = {
+        $name: name,
+        $description: description
+    };
+
+    db.run(sql, values, function(err) {
+            if(err) {
+                next(err);
+            } else {
+                db.get(`SELECT * FROM Series WHERE Series.id = ${this.lastID}`, 
+                    (err, series) => {
+                        if(err) {
+                            console.error('Error retrieving data', err);
+                            next(err);
+                        } else {
+                            console.log('data sent to DB', series);
+                            res.status(201).json({series: series});
+                        }
+                        
+                    });
+                }
+            });
+    });
+                     
+                            
+                       
+     seriesRouter.put('/:seriesId', (req, res, next) => {
+        const name = req.body.series.name;
+        const description = req.body.series.description;
+
+            if (!name || !description) {
+                return res.sendStatus(400);
+            }
+        
+        const sql = 'UPDATE Series SET name = $name, description = $description WHERE Series.id = $seriesId';
+        const values = {
+            $name: name,
+            $description: description,
+            $seriesId: req.params.seriesId
+        };
+
+        db.run(sql, values, 
+            (err) => {
+                if(err) {
+                    next(err);
+                } else {
+                    db.get(`SELECT * FROM Series WHERE Series.id = ${req.params.seriesId}`, 
+                        (err, series) => {
+                            if(err) {
+                                console.error('Error Retrieving data', err);
+                                next(err);
+                            } else {
+                                console.log('Successfully retrieved data', series);
+                                res.status(200).json({series: series});
+                            }
+                        });
+                }
+            });
+
+
+    });                   
+
+
+
+
+                        
+
 
 
 
--- migration.js
+++ migration.js
@@ -17,7 +17,21 @@ const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite'
         '`name` TEXT NOT NULL, ' +
         '`description` TEXT NOT NULL, ' +
         'PRIMARY KEY(`id`))');
+
+    db.run('CREATE TABLE IF NOT EXISTS `Issue` ( ' +
+        '`id` INTEGER NOT NULL, ' +
+        '`name` TEXT NOT NULL, ' +
+        '`issue_number` INTEGER NOT NULL, ' +
+        '`publication_date` TEXT NOT NULL, ' +
+        '`artist_id ` INTEGER NOT NULL, ' +
+        '`series_id` INTEGER NOT NULL, ' +
+        'PRIMARY KEY (`id`), ' +
+        'FOREIGN KEY(`artist_id`) REFERENCES `Artist`(`id`), ' +
+        'FOREIGN KEY(`series_id`) REFERENCES `Series`(`id`) )');
 });
+
+
+
 
 
 
