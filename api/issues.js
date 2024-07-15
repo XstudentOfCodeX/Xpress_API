62e1f16ffff1ff186c9928a9a5cfbe5adf21076b
joelhendo <hendersonjoel0@gmail.com>
2024-07-15 08:48:20 -0400

    changing machines

--- api/issues.js
+++ api/issues.js
@@ -1,22 +1,28 @@
 const express = require('express');
 const issuesRouter = express.Router({mergeParams: true});
-const sqlite3 = require('sqlite3')
+const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
+
+
 issuesRouter.get('/', (req, res, next) => {
-    db.all('SELECT * FROM Issue WHERE = Issue.series_id = $seriesId',
-        {
-            $seriesId: req.params.seriesId
-        },
-        (err, issue) => {
+    const sql = 'SELECT * FROM Issue WHERE Issue.series_id = $seriesId';
+
+    const values = {
+        $seriesId: req.params.seriesId
+    };
+
+    db.all(sql, values,
+        (err, issues) => {
         if(err){
             console.error('Error in retrieving data', err)
             next(err)
         } else {
-            res.status(200).json({issue: issue});
+            res.status(200).json({ issues: issues });
         }
     });
 });
+      
 
 
 
--- api/series.js
+++ api/series.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const seriesRouter = express.Router();
-const sqlite3 = require('sqlite3')
+const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 const issuesRouter = require('./issues.js');
 
